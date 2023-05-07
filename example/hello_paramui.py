1d5a26f8a4c1e5c071caa2c619910c1280c2ca9e
Covao / Koichi Kobayashi <kkoichimx4@hotmail.com>
2023-05-08 06:43:10 +0900

    Create hello_paramui.py

---
+++ example/hello_paramui.py
@@ -0,0 +1,25 @@
+# Hello ParamUI
+from paramui import paramui
+
+ParameterTable = [
+    ['A', 'Parameter A', 0.5, [0, 1, 0.1]],
+    ['B', 'Parameter B This is a pen', 200, [100, 500, 10]],
+    ['F1', 'Flag 1', True, []],
+    ['F2', 'Flag 2', False, []],
+    ['S1', 'Select 1', 'Two', ['One', 'Two', 'Three']],
+    ['S2', 'Select 2', 'Three', ['One', 'Two', 'Three']],
+    ['Name1', 'Name 1', 'Taro', []],
+    ['Name2', 'Name 2', 'Jiro', []],
+    ['File1', 'File 1', '', '*.py;*.txt'],
+    ['Folder1', 'Folder 1', '', 'folder'],
+    ['Run', 'Run!', [], []],
+]
+
+#Define user function
+def UsrFunc(Prm):
+  if not Prm.Run:
+    return
+  print(Prm)
+
+# Parameter display when Run button is clicked
+paramui(ParameterTable, UsrFunc)
